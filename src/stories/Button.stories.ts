a1bd161b4c7efb90779e7d8ee0cad7d73a769d4c
shah hussain <Shah.Hussain@pega.com>
2025-10-28 11:19:34 +0000

    Adding declaration file generation for type safe typescript usage

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { fn } from 'storybook/test';
+// import { fn } from 'storybook/test';
 
 import LibraryButton from '../components/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta = {
   title: 'Example/Button',
   component: LibraryButton,
   parameters: {
@@ -15,11 +15,11 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+  // args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-import path from 'node:path';
+import path from 'path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
+import dts from 'vite-plugin-dts'
+
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true, // bundle all types into a single .d.ts file
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+  ],
   build: {
     lib: {
-      entry: './src/index.ts',
+      entry: path.resolve(__dirname, 'src/index.ts'),
       // Entry file
       name: 'MyLib',
       formats: ['es', 'umd'],
